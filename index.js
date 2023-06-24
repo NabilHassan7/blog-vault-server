26e5f59f49c14ed9fda8c0c404d5cb03eba08f9a
NabilHassan7 <fakrulibnehassan@gmail.com>
2023-06-24 16:28:02 +0600

    fixed further CORS issues

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express();
+// initiating CORS
+const cors = require('cors');
+
 const port = process.env.PORT || 5000;
+
+// calling CORS function
+app.use(cors());
 
 // variable stores the data received from news categories
 const news_categories = require('./data/news_categories.json');
