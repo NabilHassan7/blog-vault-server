c576c64c3345c6da24f067f410a184536a14b7b4
NabilHassan7 <fakrulibnehassan@gmail.com>
2023-06-27 16:10:40 +0600

    deployed site using netlify serverless functions

--- functions/api.js
+++ functions/api.js
@@ -1,5 +1,9 @@
 const express = require('express')
+// importing serverless http
+const serverless = require('serverless-http');
 const app = express();
+// importing router for deploying to netlify
+const router = express.Router();
 // initiating CORS
 const cors = require('cors');
 
@@ -9,25 +13,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 // variable stores the data received from news categories
-const news_categories = require('./data/news_categories.json');
+const news_categories = require('../data/news_categories.json');
 // variable stores the data received from news 
-const news = require('./data/news.json');
+const news = require('../data/news.json');
 
 // Custom API to get info
 
 // root API call
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.send('API running');
 });
 
 // API call to get news categories
-app.get('/news-categories', (req, res) =>{
+router.get('/news-categories', (req, res) =>{
     res.send(news_categories);
 })
 
 // API call to get news according to category
 // filter used to select all matching condition
-app.get('/category/:id', (req,res) =>{
+router.get('/category/:id', (req,res) =>{
     const id = req.params.id;
     // 0 is select all news category
     if(id === '0'){
@@ -41,17 +45,21 @@ app.get('/category/:id', (req,res) =>{
 
 // API call to get specific article in details
 // find used due to unique nature of id
-app.get('/news/:id', (req,res) =>{
+router.get('/news/:id', (req,res) =>{
     const id = req.params.id;
     const selectedNews = news.find( n => n._id === id);
     res.send(selectedNews);
 })
 
 // API call to get all news
-app.get('/news', (req,res) =>{
+router.get('/news', (req,res) =>{
     res.send(news);
 })
 
-app.listen(port, () =>{
-    console.log('Base server is running on port 5000');
-})
+// nodemon function 
+// app.listen(port, () =>{
+//     console.log('Base server is running on port 5000');
+// })
+
+app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless(app);
